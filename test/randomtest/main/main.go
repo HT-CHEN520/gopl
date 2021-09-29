66dcbbc1a9f18b935c47e9d8f9497b323a63489e
HT-CHEN520 <252593241@qq.com>
2021-09-29 18:32:34 +0800

    feat:first commit

--- test/randomtest/main/main.go
+++ test/randomtest/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/HT-CHEN520/gopl/test/randomtest/utils"
 
-//我们为了生成一个随机数,还需要个rand设置一个种子
+//为了生成一个随机数,还需要个rand设置一个种子
 //time.Now().unix():返回一个从1970:01:01的0时0分0秒到现在的秒数
 //rand.seed(time. Now(). unix())
 
