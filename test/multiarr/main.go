fba7daeb25dba743f01f1b8c193bcaec4be531dd
HT-CHEN520 <252593241@qq.com>
2021-10-10 15:58:48 +0800

    feat：add multiarr

---
+++ test/multiarr/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/* 用二维数组输出图形
+0 0 0 0 0 0
+0 0 1 0 0 0
+0 2 0 3 0 0
+0 0 0 0 0 0
+*/
+
+func main() {
+	var arr [4][6]int
+
+	//初始化数组
+	arr[1][2] = 1
+	arr[2][1] = 2
+	arr[2][3] = 3
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 6; j++ {
+			//只能用print
+			fmt.Print(arr[i][j], " ")
+		}
+		fmt.Println()
+	}
+	//fmt.Println(arr)
+}
