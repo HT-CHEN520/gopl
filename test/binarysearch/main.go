84529e76b3c4d6cbafca71fdedf8c1f219dac631
HT-CHEN520 <252593241@qq.com>
2021-10-12 16:56:22 +0800

    feat:rewrite

---
+++ test/binarysearch/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+var findVal int
+
+func BinarySearch(arr *[8]int) {
+
+	var leftIndex int = 0
+	var rightIndex int = len(*arr) - 1
+	var midIndex int = (leftIndex + rightIndex) / 2
+
+	for leftIndex <= rightIndex {
+		if findVal == (*arr)[midIndex] {
+			fmt.Printf("找到该数，下标为%v", midIndex)
+			break
+		} else if findVal > (*arr)[midIndex] {
+			leftIndex = midIndex + 1
+			midIndex = (leftIndex + rightIndex) / 2
+		} else {
+			rightIndex = midIndex - 1
+			midIndex = (leftIndex + rightIndex) / 2
+		}
+	}
+
+	if leftIndex > rightIndex {
+		fmt.Println("未找到该数字！")
+	}
+
+}
+
+func main() {
+	arr := [8]int{10, 13, 25, 58, 63, 78, 89, 97}
+	fmt.Println("请输入要查询的数：")
+	fmt.Scanln(&findVal)
+	BinarySearch(&arr)
+}
