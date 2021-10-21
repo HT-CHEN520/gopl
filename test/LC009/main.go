426562ba37e67f0eaa7322b44c2ecce065912df0
HT-CHEN520 <252593241@qq.com>
2021-10-21 21:57:39 +0800

    feat:another method

--- test/LC009/main.go
+++ test/LC009/main.go
@@ -37,3 +37,20 @@ func main() {
 	result := isPalindrome(num)
 	fmt.Println(result)
 }
+
+//方法2 转成字符串比较 效率更高
+/* func isPalindrome(x int) bool {
+	if x < 0 || x%10 == 0 &&x!=0 {
+		return false
+	} else {
+		str := strconv.Itoa(x)
+		i := 0
+		for j := len(str) - 1; i < j; j-- {
+			if str[i] != str[j] {
+				return false
+			}
+			i++
+		}
+	}
+	return true
+} */
